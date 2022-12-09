908c4f9f83cd7074f2021b6a0a97d8090a07ffe5
ReeseKenn <yanbaorui1997@163.com>
2022-12-09 14:18:55 +0800

    初始化router, 添加错误页面

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ErrorPage } from './components/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>root</div>,
+    errorElement: <ErrorPage/>
+  }
+])
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
@@ -8,6 +17,6 @@ const root = ReactDOM.createRoot(div)
 root.render(
   // 检查代码是否有错误的标签
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
