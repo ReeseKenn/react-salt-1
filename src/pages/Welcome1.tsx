6ecf5ddffe1c753659a67ed7fb4d255a104228a0
ReeseKenn <yanbaorui1997@163.com>
2023-02-25 23:04:55 +0800

    初步学习css modules写法

--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,7 +1,14 @@
 import { NavLink } from 'react-router-dom'
+import c from 'classnames'
+import s from './Welcome.module.scss'
 
 export const Welcome1: React.FC = () => {
   return (
-    <div style={{ border: '1px solid red' }}>1<NavLink to="/welcome/2">下一页</NavLink></div>
+    <div className={c(s.wrapper, s.test)}>
+      <header>
+        <h2>海盐账本</h2>
+      </header>
+      <NavLink to="/welcome/2">下一页</NavLink>
+    </div>
   )
 }
