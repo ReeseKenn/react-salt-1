7092eede27328dde245484d61506bc9413f0430d
ReeseKenn <yanbaorui1997@163.com>
2023-02-26 22:16:39 +0800

    初步学习styled components写法

--- src/pages/Welcome2.tsx
+++ src/pages/Welcome2.tsx
@@ -1,7 +1,29 @@
 import { NavLink } from 'react-router-dom'
-
+import styled from 'styled-components'
+const Header = styled.div`
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 40px;
+  color: #60A3BC;
+  &:hover{
+    border: 1px solid #60A3BC;
+  }
+`
+const RedHeader = styled(Header)`
+  background-color: #999;
+`
 export const Welcome2: React.FC = () => {
   return (
-    <div style={{ border: '1px solid red' }}>2<NavLink to="/welcome/3">下一页</NavLink></div>
+    <div>
+      <Header>
+        海盐账本
+      </Header>
+      <RedHeader>
+        <NavLink to="/welcome/3">下一页</NavLink>
+      </RedHeader>
+      </div>
   )
 }
