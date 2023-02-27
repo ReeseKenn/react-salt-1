740f3149ef0699d86699d5135e5ddfec90964337
ReeseKenn <yanbaorui1997@163.com>
2023-02-27 21:56:37 +0800

    初步学习unocss写法

--- src/pages/Welcome3.tsx
+++ src/pages/Welcome3.tsx
@@ -2,6 +2,11 @@ import { NavLink } from 'react-router-dom'
 
 export const Welcome3: React.FC = () => {
   return (
-    <div style={{ border: '1px solid red' }}>3<NavLink to="/welcome/4">下一页</NavLink></div>
+    <div>
+      <header hover:b-1 hover:b-red b-primary b-3 text-primary flex>
+         海盐账本
+      </header>
+      <NavLink to="/welcome/4">下一页</NavLink>
+      </div>
   )
 }
---
+++ uno.config.ts
@@ -0,0 +1,26 @@
+import {
+  defineConfig, presetAttributify, presetIcons,
+  presetTypography, presetUno, transformerAttributifyJsx
+} from 'unocss'
+
+export default defineConfig({
+  theme: {
+    colors: {
+      primary: '#60A3BC'
+    }
+  },
+  shortcuts: {
+  },
+  safelist: [],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      extraProperties: { 'display': 'inline-block', 'vertical-align': 'middle' },
+    }),
+    presetTypography(),
+  ],
+  transformers: [
+    transformerAttributifyJsx()
+  ],
+})
