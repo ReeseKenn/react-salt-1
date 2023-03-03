3cdbb896596fbf05aa35c050e59faa8329ed699a
ReeseKenn <yanbaorui1997@163.com>
2023-03-03 21:48:03 +0800

    初步添加手势滑动触发动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,18 +1,39 @@
 import { animated, useTransition } from '@react-spring/web'
 import type { ReactNode } from 'react'
-import { useRef } from 'react'
-import { Link, useLocation, useOutlet } from 'react-router-dom'
+import { useEffect, useRef } from 'react'
+import { Link, useLocation, useNavigate, useOutlet } from 'react-router-dom'
 import logo from '../assets/icons/salt.svg'
+import { useSwipe } from '../hooks/useSwipe'
 
 export const WelcomeLayout: React.FC = () => {
+  const transition = useRef(false)
   const map = useRef<Record < string, ReactNode >>({})
   const location = useLocation() // 获取当前地址栏的信息
   const outlet = useOutlet()
   map.current[location.pathname] = outlet
+  const main = useRef(null)
+  const { direction } = useSwipe(main, { onTouchStart: e => e.preventDefault() })
+  const linkMap: Record<string, string> = {
+    '/welcome/1': '/welcome/2',
+    '/welcome/2': '/welcome/3',
+    '/welcome/3': '/welcome/4',
+    '/welcome/4': '/start'
+  }
+  const nav = useNavigate()
+  useEffect(() => {
+    if (transition.current === true) { return }
+    if (direction === 'left') {
+      transition.current = true
+      nav(linkMap[location.pathname])
+    }
+  }, [direction])
   const transitions = useTransition(location.pathname, {
     from: { transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
     enter: { transform: 'translateX(0%)' },
     leave: { transform: 'translateX(-100%)' },
+    onRest: () => {
+      transition.current = false
+    },
     config: { duration: 500 }
   })
   return (
@@ -21,7 +42,7 @@ export const WelcomeLayout: React.FC = () => {
         <img src={logo} w-54px h-54px/>
         <h2 text-primary>海盐账本</h2>
       </header>
-      <main grow-1 relative w="100%">
+      <main grow-1 relative w="100%" ref={main}>
         {transitions((style, pathname) =>
         <animated.div key={pathname} style={style} absolute w="100%" h="100%">
           <div grow-1 flex flex-col justify-center items-center >
