658591303481c3777c33a2e392eea3105b2ffad0
ReeseKenn <yanbaorui1997@163.com>
2023-03-10 10:30:48 +0800

    welcome4无法进入问题修复

--- src/pages/Welcome4.tsx
+++ src/pages/Welcome4.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react'
 import pic from '../assets/icons/w4.svg'
 import { useLocalStore } from '../stores/useLocalStore'
 
 export const Welcome4: React.FC = () => {
   const { setHasRead } = useLocalStore()
-  setHasRead(true)
+  useEffect(() => {
+    setHasRead(true)
+  }, [])
+
   return <>
       <img src={pic}/>
       <h3 text-primary>云备份，不惧数据丢失</h3>
