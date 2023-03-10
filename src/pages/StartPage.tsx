e2fc14149d61d8d2feffc30b72bbda3799ff0f42
ReeseKenn <yanbaorui1997@163.com>
2023-03-10 11:16:23 +0800

    引入swr,axios

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,12 +1,21 @@
+import axios from 'axios'
+import useSWR from 'swr'
 import pic from '../assets/icons/w1.svg'
 import add from '../assets/icons/add.svg'
+
+const fetcher = (path: string) => {
+  return axios.get(path)
+}
 export const StratPage: React.FC = () => {
+  fetcher('/api/')
+  const { data, error, isValidating, mutate } = useSWR('/api/api/v1/items', fetcher)
+  const isLoading = !data && !error
   return <div>
   <div flex justify-center items-center>
     <img mt-16vh mb-16vh width="128" height="128" src={pic} />
   </div>
   <div px-16px>
-    <button s-btn w='100%'>开始记账</button>
+    <button s-btn w='100%'>开始记账{isLoading}</button>
   </div>
   <button p-4px w-56px h-56px bg-primary rounded="50%" b-none text-white
     text-6xl fixed bottom-16px right-16px flex justify-center items-center>
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,19 @@ export default defineConfig({
   plugins: [
     Unocss(),
     react()
-  ]
+  ],
+  server: {
+    host: '0.0.0.0', // 打开显示本地地址
+    open: true, // 是否自动启动浏览器
+    port: 5173, // 端口号
+    // 代理解决跨域
+    proxy: { // 本地开发环境通过代理实现跨域
+      // 正则表达式写法
+      '/api': {
+        target: 'http://150.158.148.72:3000', // 后端服务实际地址
+        changeOrigin: true, // 开启代理
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
