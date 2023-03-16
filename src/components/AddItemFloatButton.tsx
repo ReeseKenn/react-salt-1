97280349272090a00ae8d09749993b4757e24f3b
ReeseKenn <yanbaorui1997@163.com>
2023-03-16 10:25:53 +0800

    封装Icon组件，并替换，解决组件样式覆盖问题，unocss样式优先级及css where选择器低优先级了解

--- src/components/AddItemFloatButton.tsx
+++ src/components/AddItemFloatButton.tsx
@@ -1,10 +1,13 @@
+import { Icon } from './Icon'
+
 export const AddItemFloatButton: React.FC = () => {
   return (
     <button p-4px w-56px h-56px bg-primary rounded="50%" b-none text-white
     text-2xl fixed bottom-16px right-16px flex justify-center items-center>
-      <svg style={{ fill: '#fff', width: '1.2em', height: '1.2em' }}>
+      {/* <svg style={{ fill: '#fff', width: '1.2em', height: '1.2em' }}>
         <use xlinkHref="#add"></use>
-      </svg>
+      </svg> */}
+      <Icon className='c-white' name="add"/>
     </button>
   )
 }
