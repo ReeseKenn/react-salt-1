0f11eed82044e1c0aba65875bb8ce8d97b76b194
ReeseKenn <yanbaorui1997@163.com>
2023-03-21 22:41:54 +0800

    init itemspage

---
+++ src/pages/ItemsPage.tsx
@@ -0,0 +1,10 @@
+import { AddItemFloatButton } from '../components/AddItemFloatButton'
+
+export const ItemsPage: React.FC = () => {
+  return (
+    <div>
+      列表页
+      <AddItemFloatButton />
+    </div>
+  )
+}
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,7 @@
 import { Outlet, createBrowserRouter } from 'react-router-dom'
 import { RedirectToWelcome1 } from '../components/RedirectToWelcome1'
 import { Root } from '../components/Root'
+import { ItemsPage } from '../pages/ItemsPage'
 import { StratPage } from '../pages/StartPage'
 import { welcomeRoutes } from './welcomeRoutes'
 
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
       {
         path: 'start',
         element: <StratPage/>
+      },
+      {
+        path: 'items',
+        element: <ItemsPage/>
       },
       welcomeRoutes
     ]
