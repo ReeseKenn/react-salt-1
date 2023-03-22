d2e71b03103d309e111195dfc0314299b16a37c9
ReeseKenn <yanbaorui1997@163.com>
2023-03-22 23:11:31 +0800

    初始化ItemsPage

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,9 +1,23 @@
+import styled from 'styled-components'
 import { AddItemFloatButton } from '../components/AddItemFloatButton'
+import { TimeRangePicker } from '../components/TimeRangePicker'
+import { Topnav } from '../components/Topnav'
+import { ItemsList } from './ItemsPage/ItemsList'
+import { ItemsSummary } from './ItemsPage/ItemsSummary'
+
+const Div = styled.div`
+  background: linear-gradient(0deg, #82ccdd 0%, #60A3BC 100%);
+`
 
 export const ItemsPage: React.FC = () => {
   return (
     <div>
-      列表页
+      <Div>
+        <Topnav />
+        <TimeRangePicker/>
+      </Div>
+      <ItemsSummary/>
+      <ItemsList/>
       <AddItemFloatButton />
     </div>
   )
