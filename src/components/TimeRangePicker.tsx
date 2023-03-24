438bef9f8d3fa4031a790fd66ebe4d71f9a6fe31
ReeseKenn <yanbaorui1997@163.com>
2023-03-24 23:56:29 +0800

    完成timepicker样式

--- src/components/TimeRangePicker.tsx
+++ src/components/TimeRangePicker.tsx
@@ -1,4 +1,11 @@
 export const TimeRangePicker: React.FC = () => {
-  return <div>TimeRangePicker </div>
+  return (
+  <ol flex text-white cursor-pointer children-p-16px>
+    <li>本月</li>
+    <li>上月</li>
+    <li>今年</li>
+    <li>自定义时间</li>
+  </ol>
+  )
 }
 
