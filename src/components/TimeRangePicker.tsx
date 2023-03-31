d9325757af874d4920d4bbb3ec80401895f582f9
ReeseKenn <yanbaorui1997@163.com>
2023-03-31 09:41:49 +0800

    优化

--- src/components/TimeRangePicker.tsx
+++ src/components/TimeRangePicker.tsx
@@ -4,13 +4,13 @@ interface Props {
   selected: TimeRange
   onSelected: (selected: TimeRange) => void
 }
+const timeRanges: { key: TimeRange; label: string }[] = [
+  { key: 'thisMonth', label: '本月' },
+  { key: 'lastMonth', label: '上月' },
+  { key: 'thisYear', label: '今年' },
+  { key: 'custom', label: '自定义时间' },
+]
 export const TimeRangePicker: React.FC<Props> = ({ selected, onSelected }) => {
-  const timeRanges: { key: TimeRange; label: string }[] = [
-    { key: 'thisMonth', label: '本月' },
-    { key: 'lastMonth', label: '上月' },
-    { key: 'thisYear', label: '今年' },
-    { key: 'custom', label: '自定义时间' },
-  ]
   return (
   <ol flex text-white cursor-pointer children-p-16px>
     {timeRanges.map(item => <li key={item.key} className={item.key === selected ? s.selected : ''}
